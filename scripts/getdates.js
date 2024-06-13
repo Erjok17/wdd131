cc7a4bf43fcb15d8e354854f1d43b3f9780ab305
Erjok17 <mrerjok@gmail.com>
2024-06-13 23:44:01 +0300

    home

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+// getdates.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    // Get the last modified date
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = "Last Modified: " + lastModified;
+});
